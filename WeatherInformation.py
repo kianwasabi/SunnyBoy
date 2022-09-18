775c45edfd5d286b7d80c29c7e0f354144fc4fe9
kianwasabi <55065075+kianwasabi@users.noreply.github.com>
2022-09-18 22:05:55 +0200

    modified class wind, sun & city

--- WeatherInformation.py
+++ WeatherInformation.py
@@ -1,3 +1,4 @@
+# --- Include Libraries ---
 import requests
 import os
 import PySimpleGUI as sg
@@ -10,86 +11,33 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 #Class City, Sub & Wind
 class City:
-    def __init__(self):
-        self.name = None
-        self.temperatur = None
-        self.timezone = None
-        self.time_api_data_calculation = None
-        self.weather_description = None
-        self.longitute = None
-        self.latitude = None
+    def __init__(self, name, temperatur, timezone, time_api_data_calculation, weather_description, longitute, latitude):
+        self.name = name
+        self.temperatur = temperatur
+        self.timezone = timezone
+        self.time_api_data_calculation = time_api_data_calculation
+        self.weather_description = weather_description
+        self.longitute = longitute
+        self.latitude = latitude
 class Sun:
-    def __init__(self):
-        self.cloudiness = None
-        self.visibility = None
-        self.rise = None
-        self.set = None
+    def __init__(self, cloudiness, visibility, sunrise, sunset):
+        self.cloudiness = cloudiness
+        self.visibility = visibility
+        self.rise = sunrise
+        self.set = sunset
         self.azimuth = None
         self.azimuth_point = None
         self.elevation = None
-class Wind:
-    def __init__(self):
-        self.speed = None
-        self.direction = None
-        self.direction_point = None
-
-def callOpenWeatherAPI(city_name):
-    def apiLogin (): 
-        filename = 'OpenWeatherMap.txt'  
-        try:
-            with open(filename) as file:
-                lines = file.readlines()
-            user_api = lines[-1]
-            return user_api
-        except FileNotFoundError as err: 
-            print(filename,"not found")
-            raise SystemExit(err)
-        else: 
-            return user_api
-    user_api = apiLogin()
-
-    #Call OpenWeatherMap API 
-    try: 
-        complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+city_name+"&appid="+user_api
-        api_link = requests.get(complete_api_link)
-        api_data = api_link.json()
-        api_link.raise_for_status()
-    except Exception as err: 
-            if api_data['cod'] == '404':
-                print("Invalide City. Please check city name.")
-                raise SystemExit(err)
-    else: 
-        return api_data
-
-#Convert Degree to Point, North=0°
-def  convertDegreetoPoint(degree):
-    if(degree>337.5):
-        return 'N'
-    if(degree>292.5):
-        return 'NW'
-    if(degree>247.5):
-        return 'W'
-    if(degree>202.5):
-        return 'SW'
-    if(degree>157.5):
-        return 'S'
-    if(degree>122.5):
-        return 'SE'
-    if(degree>67.5):
-        return 'E'
-    if(degree>22.5):
-        return 'NE'
-    return 'N' 
 
 # Get the Sun's apparent location in the sky
 def sunpos(when,tz,location,refraction):
-    # inner function into_range 
+   # inner function into_range 
     def into_range(x, range_min, range_max):
         shiftedx = x - range_min
         delta = range_max - range_min
         # Close Encounters latitude, longitude
         return (((shiftedx % delta) + delta) % delta) + range_min
-    #Extract the passed data
+        #Extract the passed data
     year = when.year
     month = when.month
     day = when.day
@@ -149,6 +97,61 @@ def sunpos(when,tz,location,refraction):
         elevation += (1.02 / tan(targ)) / 60
         return (round(azimuth, 2)), round(elevation, 2)
 
+class Wind:
+    def __init__(self, wind_speed, wind_direction):
+        self.speed = wind_speed
+        self.direction = wind_direction
+        self.direction_point = None
+
+def callOpenWeatherAPI(city_name):
+    def apiLogin (): 
+        filename = 'OpenWeatherMap.txt'  
+        try:
+            with open(filename) as file:
+                lines = file.readlines()
+            user_api = lines[-1]
+            return user_api
+        except FileNotFoundError as err: 
+            print(filename,"not found")
+            raise SystemExit(err)
+        else: 
+            return user_api
+    user_api = apiLogin()
+
+    #Call OpenWeatherMap API 
+    try: 
+        complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+city_name+"&appid="+user_api
+        api_link = requests.get(complete_api_link)
+        api_data = api_link.json()
+        api_link.raise_for_status()
+    except Exception as err: 
+            if api_data['cod'] == '404':
+                print("Invalide City. Please check city name.")
+                raise SystemExit(err)
+    else: 
+        return api_data
+
+#Convert Degree to Point, North=0°
+def  convertDegreetoPoint(degree):
+    if(degree>337.5):
+        return 'N'
+    if(degree>292.5):
+        return 'NW'
+    if(degree>247.5):
+        return 'W'
+    if(degree>202.5):
+        return 'SW'
+    if(degree>157.5):
+        return 'S'
+    if(degree>122.5):
+        return 'SE'
+    if(degree>67.5):
+        return 'E'
+    if(degree>22.5):
+        return 'NE'
+    return 'N' 
+
+
 def printinTerminal(city1,wind1,sun1):
     print ("-------------------------------------------------------------")
     print ("Location             :",format(city1.name.upper()))
@@ -169,34 +172,30 @@ def printinTerminal(city1,wind1,sun1):
     print ("Wind Direction       :",wind1.direction,"°","(",wind1.direction_point,")")
 
 def main(): 
-    #init city, wind and sun object
-    city1 = City()
-    wind1 = Wind()
-    sun1 = Sun()
-    #Get User Location 
-    city1.name = input("Enter city name: ")
-    #Get Weather Information from OpenWeatherMap.org/api
-    api_data = callOpenWeatherAPI(city1.name)
-    #Assign general weather Informations for the choosen location
-    city1.longitute = api_data['coord']['lat']
-    city1.latitude = api_data['coord']['lon']
-    city1.temperatur = ((api_data['main']['temp']) - 273.15) #°F to °C
-    city1.timezone = api_data['timezone']/60/60 #Shift in seconds from UTC 
-    city1.time_api_data_calculation = datetime.fromtimestamp(api_data['dt'])
-    city1.weather_description = api_data['weather'][0]['description']
-    #Assign Informations about the sun
-    sun1.rise = datetime.utcfromtimestamp(api_data['sys']['sunrise']).strftime("%H:%M:%S")
-    sun1.set = datetime.utcfromtimestamp(api_data['sys']['sunset']).strftime("%H:%M:%S") 
-    sun1.cloudiness = api_data['clouds']['all']
-    sun1.visibility = api_data['visibility']  
+    cityname = input("Enter city name: ")
+    # --- Call API ---
+    api_data = callOpenWeatherAPI(cityname)
+    longitute = api_data['coord']['lat']
+    latitude = api_data['coord']['lon']
+    temperatur = ((api_data['main']['temp']) - 273.15) #°F to °C
+    timezone = api_data['timezone']/60/60 #Shift in seconds from UTC 
+    time_api_data_calculation = datetime.fromtimestamp(api_data['dt'])
+    weather_description = api_data['weather'][0]['description']
+    sunrise = datetime.utcfromtimestamp(api_data['sys']['sunrise']).strftime("%H:%M:%S")
+    sunset = datetime.utcfromtimestamp(api_data['sys']['sunset']).strftime("%H:%M:%S") 
+    cloudiness = api_data['clouds']['all']
+    visibility = api_data['visibility']
+    wind_speed = api_data['wind']['speed']
+    wind_direction = api_data['wind']['deg']
+    # --- Initialization Objects ---
+    city1 = City(cityname, temperatur, timezone, time_api_data_calculation, weather_description, longitute, latitude)
+    sun1 = Sun(cloudiness, visibility, sunrise, sunset)
+    wind1 = Wind(wind_speed, wind_direction)
     sun1.azimuth, sun1.elevation = sunpos(city1.time_api_data_calculation, city1.timezone, 
                                         (city1.latitude, city1.longitute), True)
     sun1.azimuth_point = convertDegreetoPoint(sun1.azimuth)
-    #Assign Informations about the wind
-    wind1.speed = api_data['wind']['speed']
-    wind1.direction = api_data['wind']['deg']
     wind1.direction_point = convertDegreetoPoint(wind1.direction)
-    #Print all in Terminal
+    # --- Print in Terminal ---
     printinTerminal(city1,wind1,sun1)
 
 if __name__ == '__main__':
