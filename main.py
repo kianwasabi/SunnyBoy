765ae4054ce517c167b5b0a0a6e62bd0406fcac8
kianwasabi <55065075+kianwasabi@users.noreply.github.com>
2022-10-08 16:45:33 +0200

    did some stuff

--- main.py
+++ main.py
@@ -1,48 +1,20 @@
 #include libraries
 import os
 import os.path
-import requests
-import PySimpleGUI as sg
+#import PySimpleGUIWeb as sg
 from datetime import datetime
 import pytz
 from timezonefinder import TimezoneFinder
+# import modules
+from weatherAPI_call import callOpenWeatherAPI
 # import classes
 from sun_def import Sun 
 from weather_def import Weather
 from wind_def import Wind
- 
-#clear interpreter console
-os.system('cls' if os.name == 'nt' else 'clear')
-
-def callOpenWeatherAPI(city_name):
-    def apiLogin (): 
-        filename = 'OpenWeatherMap.txt'  
-        try:
-            with open(filename) as file:
-                lines = file.readlines()
-            user_api = lines[-1]
-            return user_api
-        except FileNotFoundError as err: 
-            print(filename,"not found")
-            raise SystemExit(err)
-        else: 
-            return user_api
-    user_api = apiLogin()
-
-    #Call OpenWeatherMap API 
-    try: 
-        complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+city_name+"&appid="+user_api
-        api_link = requests.get(complete_api_link)
-        api_data = api_link.json()
-        api_link.raise_for_status()
-    except Exception as err: 
-            if api_data['cod'] == '404':
-                print("Invalide City. Please check city name.")
-                raise SystemExit(err)
-    else: 
-        return api_data
 
 def printinTerminal(weather,wind,sun):
+    #clear interpreter console
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("---------------------------------------------------------------")
     print("Location: ",weather.getLocationName(),"(",weather.getLongitude(),",",weather.getLatitude(),")")
     print("Time:", weather.getDate(),weather.getTime())
@@ -51,7 +23,7 @@ def printinTerminal(weather,wind,sun):
     print("---------------------------------------------------------------")
 
 def main(): 
-    #bootcCall API
+    #Call API
     cityname = input("Enter city name: ")
     api_data = callOpenWeatherAPI(cityname)
     latitude = api_data['coord']['lat']
@@ -62,25 +34,25 @@ def main():
     #time_api = api_data['dt'] #api time of data caluclation, timestamp unix, UTC
     #time_in_tz = time_api + timezone
     #time_in_tz = datetime.fromtimestamp(time_in_tz)
-    result = TimezoneFinder().timezone_at(lng=longitude,lat=latitude)
-    tz = pytz.timezone(result)
+    timezone_at = TimezoneFinder().timezone_at(lng=longitude,lat=latitude)
+    tz = pytz.timezone(timezone_at)
     time_in_tz = datetime.now(tz)
-    timestamp_sunrise = api_data['sys']['sunrise']
-    timestamp_sunrise = timestamp_sunrise + (timezone-7200)
+    timestamp_sunrise = api_data['sys']['sunrise'] #timezone in seconds unix, UTC
+    timestamp_sunrise = timestamp_sunrise + (timezone-7200) # edit sunrise time in timezone 
     timestamp_sunrise = datetime.fromtimestamp(timestamp_sunrise)
     timestamp_sunset = api_data['sys']['sunset']
     timestamp_sunset = timestamp_sunset + (timezone-7200)
     timestamp_sunset = datetime.fromtimestamp(timestamp_sunset)
-    timezone = round(api_data['timezone']/60/60) #timezone shifted in seconds from unix, UTC
+    timezone = round(api_data['timezone']/60/60) #timezone shifted in seconds, unix, UTC
     cloudiness = api_data['clouds']['all']
     visibility = api_data['visibility']
     wind_speed = api_data['wind']['speed']
     wind_direction = api_data['wind']['deg']
-    #Initialization Objects
+    #initialization Objects
     weather = Weather(temperatur,weather_description, cloudiness, visibility,cityname, longitude, latitude, time_in_tz, timezone)
     wind = Wind(wind_speed, wind_direction, cityname, longitude, latitude, time_in_tz,timezone)
     sun = Sun(timestamp_sunrise,timestamp_sunset, cityname, longitude, latitude, time_in_tz,timezone)
-    #Plot in terminal
+    #print in terminal
     printinTerminal(weather,wind,sun)
     
 if __name__ == '__main__':
---
+++ weatherAPI_call.py
@@ -0,0 +1,29 @@
+import requests
+
+def callOpenWeatherAPI(city_name):
+    def apiLogin (): 
+        filename = 'OpenWeatherMap.txt'  
+        try:
+            with open(filename) as file:
+                lines = file.readlines()
+            user_api = lines[-1]
+            return user_api
+        except FileNotFoundError as err: 
+            print(filename,"not found")
+            raise SystemExit(err)
+        else: 
+            return user_api
+    user_api = apiLogin()
+
+    #Call OpenWeatherMap API 
+    try: 
+        complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+city_name+"&appid="+user_api
+        api_link = requests.get(complete_api_link)
+        api_data = api_link.json()
+        api_link.raise_for_status()
+    except Exception as err: 
+            if api_data['cod'] == '404':
+                print("Invalide City. Please check city name.")
+                raise SystemExit(err)
+    else: 
+        return api_data
